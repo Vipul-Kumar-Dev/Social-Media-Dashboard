a19a852d4052c5f2290b2899a191e5bd223616e8
Vipul Kumar <Vipul.Kumar.Dev@outlook.com>
2025-09-06 10:52:10 +0530

    .

--- core/models.py
+++ core/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -68,3 +70,10 @@ class Profile(models.Model):
             self.is_reddit_linked(),
             self.is_youtube_linked()
         ])
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    else:
+        instance.profile.save()
